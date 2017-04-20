f4f020e72f921fc105896005b35dca2750999876
Gorka <gorka@aquigorka.com>
2017-04-20 23:24:45 +0200

    Adds shell utils

---
+++ utils.sh
@@ -0,0 +1,16 @@
+# formatted grep
+# params: search query (regex) & path to look in (folder)
+# returns: stdout formatted search results similar to sublime text's find all
+# file name (# matches in file) + context with hit
+
+sF() {
+  mySearch=$1
+  myPath=$2
+
+  grep -lr $mySearch $myPath | while read file
+  do
+    echo "\n"
+    echo -e "\e[34m$file ($(grep -c $mySearch $file)):\e[0m"
+    grep -nh -A3 -B1 --color=always $mySearch $file
+  done
+}
