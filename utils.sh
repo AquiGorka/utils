1226bdee41347edffae43de4f53f77d7ea063f93
Gorka <gorka@aquigorka.com>
2017-05-05 09:27:40 +0200

    Updates shell utils and modifies javascript utils filename

--- utils.sh
+++ utils.sh
@@ -2,20 +2,30 @@
 # params: search query (regex) & path to look in (folder includes subfolders)
 # returns: stdout formatted search results similar to sublime text's find all
 # file name (# matches in file) + context with hit
-# usage: sF "regex" path_to_a_dir
+# usage: sF "regex" path_to_a_dir [optional params: eg -i (avoid case matching)]
 
 sF() {
   mySearch=$1
   myPath=$2
+  myParams=$3
 
-  grep -lr $mySearch $myPath | while read file
+  grep -lr $myParams $mySearch $myPath | while read file
   do
     echo "\n"
-    echo -e "\e[34m$file ($(grep -c $mySearch $file)):\e[0m"
-    grep -nh -A3 -B1 --color=always $mySearch $file
+    echo -e "\e[34m$file ($(grep -c $myParams $mySearch $file)):\e[0m"
+    grep -nh -A3 -B1 --color=always $myParams $mySearch $file
   done
 }
 
 # Run a docker container with interactive shell and read-only from local folder
 # remove :ro to give write access (changes to the files from within the container will be reflected to the local filesystem)
-docker run -it --volume=/Users/my-user/local-folder:/container-folder:ro --workdir="/container-folder" --memory=4g --memory-swap=4g --entrypoint=/bin/bash ubuntu:latest
+# docker run -it --volume=/Users/my-user/local-folder:/container-folder:ro --workdir="/container-folder" --entrypoint=/bin/bash ubuntu:latest
+# usage: runD /Users/my-user/local-folder /container-folder /bin/bash ubuntu:latest
+runD() {
+  myLocalPath=$1
+  myContainerPath=$2
+  myEntryPoint=$3
+  myImage=$4
+
+  docker run -it --volume "$myLocalPath:$myContainerPath" --workdir $myContainerPath --entryoint $myEntryPoint $myImage
+}
